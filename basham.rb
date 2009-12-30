db1998669b1cddb79035f69ccdabfcc6bbbdc324
Chris Basham <chris@bash.am>
2009-12-30 02:01:31 -0600

    Built basic page controllers. Auto titles. Auto CSS search based on slug.

--- basham.rb
+++ basham.rb
@@ -2,20 +2,87 @@ require 'rubygems'
 require 'sinatra'
 require 'Djerb'
 
+portfolio = [
+  { :title => 'Waterwall' },
+  { :title => 'Daybreak' },
+  { :title => 'PING Platform' } ]
+  
+helpers do
+  
+  def render( template, path = 'views', ext = '.erb' )
+    template = path + '/' + template + ext
+    Djerb.run template, lambda{}
+  end
+  
+  def title( label = '' )
+    title = 'Chris Basham'
+    if ( label != '' )
+      title = label + ' | ' + title
+    end
+    return title
+  end
+  
+  def smartTitle()
+    return title uri.capitalize
+  end
+  
+  def uri()
+    return request.path_info.match(/(.*)\/(.*)/).captures[1]
+  end
+  
+  def slug( label )
+    return label.downcase.gsub(' ', '-')
+  end
+  
+end
+
+before do
+
+  @css = []
+  @url = '/assets/css/slug/' + uri + '.css'
+  if File.exists?( 'public' + @url )
+    @css = [@url]
+  end
+  
+  @title = smartTitle
+end
+
 get '/' do
-	@title = 'woots'
-	
-	Djerb.run 'views/base.erb', lambda{}
+	render 'base'
 end
 
 get '/portfolio' do
-	@title = 'Daybreak'
-	
-	Djerb.run 'views/index.erb', lambda{}
+  @portfolio = portfolio
+	render 'portfolio'
+end
+
+get '/portfolio/:item' do
+  passed = false
+  portfolio.each do |item|
+    @slug = slug item[:title]
+    if uri == @slug
+      passed = true
+      @title = title item[:title]
+      break
+    end
+  end
+  pass if !passed
+  render 'portfolio/' + @slug
+end
+
+get '/resume' do
+	render 'resume'
 end
 
 get '/about' do
-	@title = 'About'
-	
-	Djerb.run 'views/about.erb', lambda{}
+	render 'about'
+end
+
+get '/colophon' do
+	render 'colophon'
+end
+
+not_found do
+  @title = title '404'
+	render '404'
 end
